24f0387402b19e9d72b9cad63d0efcb1cf493fc8
William R. J. Ribeiro <will@williamrjribeiro.com>
2021-09-27 12:50:17 +0100

    Multiple fixes
    - use NVM with a plugin for ease-of-life to handle all Node stuff
    
    - Fix Spaceship theme installation

--- dev.sh
+++ dev.sh
@@ -9,16 +9,24 @@ echo "[dev] installing dev dependencies..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "ZSH_DISABLE_COMPFIX=true\n$(cat ~/.zshrc)" > ~/.zshrc
 
+## install a bunch of dev dependencies using BREW
 brew bundle --file dev.Brewfile
 
 $(brew --prefix)/opt/fzf/install
 
-echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+# Easy NVM install https://github.com/lukechilds/zsh-nvm
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 
-sed -i "" 's/ZSH_THEME="robbyrussell"/# ZSH_THEME="robbyrussell"/g' ~/.zshrc
-sed -i "" 's/plugins=(git)/plugins=(git docker docker-compose aws fzf)/g' ~/.zshrc
+## install Yarn with NPM AFTER NVM so the Node versions match
+npm i -g yarn
 
+## Change THEME
+sed -i "" 's/ZSH_THEME="robbyrussell"/# ZSH_THEME="spaceship"/g' ~/.zshrc
+
+## Add Plugins
+sed -i "" 's/plugins=(git)/plugins=(git docker docker-compose aws fzf zsh-nvm)/g' ~/.zshrc
+
+## Add more fonts to the system
 cp /System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts/*.otf ~/Library/Fonts/
 
 cp .gitconfig $HOME
@@ -26,11 +34,13 @@ cp .gitconfig $HOME
 # Set Visual Studio Code as preferred text editor
 gh config set editor "code -w"
 
+
+echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
 echo 'alias lle="exa --long --header --git --icons"' >> ~/.zshrc
 
-npm install -g spaceship-zsh-theme
 echo 'SPACESHIP_BATTERY_SHOW="false"' >> ~/.zshrc
-echo 'SPACESHIP_EXIT_CODE_SHOW="true"' >> ~/.zshrc
 
 # setup dev env
 DEV_PATH=$HOME/workspace
@@ -39,4 +49,3 @@ echo "[dev] created default workspace at $DEV_PATH"
 
 sudo xcode-select --install && sleep 1
 osascript -e 'tell application "System Events"' -e 'tell process "Install Command Line Developer Tools"' -e 'keystroke return' -e 'click button "Agree" of window "License Agreement"' -e 'end tell' -e 'end tell'
-
